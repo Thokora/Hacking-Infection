5b063d0c92bc103a5be7cd8730f5e4874a864d51
thunderbol <jdvid.bermudez@gmail.com>
2019-03-25 01:18:07 -0500

    DisparoImplementadoError

---
+++ Hacking Infection/Assets/Pruebas/Scripts/MoveBulletShooter.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBulletShooter : MonoBehaviour
+{
+
+
+    public float speed;
+
+
+
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        rb.velocity = transform.forward * speed;
+       
+    }
+}
---
+++ Hacking Infection/Assets/Pruebas/Scripts/WeaponsControllerEnemie.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponsControllerEnemie : MonoBehaviour
+{
+
+
+    public GameObject shot;
+    public Transform shotSpawn;
+
+    public float delay; //Timepo de espera para emepzar a disparar
+    public float fireRate;  //Tiempo de espera volver a llamar el disparo Fire();
+
+
+    private AudioSource audioSource;
+
+
+
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+
+    void Start()
+    {
+        InvokeRepeating("Fire", delay, fireRate); //InvokeRepeating lo que hace es generar esperas
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        audioSource.Play();
+    }
+}
