e6eab51ecbdfb91d785ff9175362370f0410419e
thunderbol <jdvid.bermudez@gmail.com>
2019-04-08 17:22:04 -0500

    Revert "enemigos"
    This reverts commit c8fa68d19591100f03df7cec1eaf06c40985470b.

--- Hacking Infection/Assets/Pruebas/Scripts/ExplotionEnemis.cs
+++ Hacking Infection/Assets/Pruebas/Scripts/ExplotionEnemis.cs
@@ -4,37 +4,20 @@ using UnityEngine;
 
 public class ExplotionEnemis : MonoBehaviour
 {
-
-    [Header("Agregar fuerza y destrucción de las partes")]
-    public float timeDelay;
     public float minForce;
     public float maxForce;
     public float radius;
-    public GameObject disparo;
-
-    private float timeEspera;
-
-
-    private void Start()
-    {
-        Explode();
-    }
 
     public void Explode()
     {
         foreach (Transform t in transform)
         {
-
-            
             var rb = t.GetComponent<Rigidbody>();
 
             if(rb != null)
             {
                 rb.AddExplosionForce(Random.Range(minForce, maxForce), transform.position, radius);
             }
-
-            Destroy(t.gameObject, timeDelay);
-            Destroy(disparo, timeDelay);
 
         }
     }
