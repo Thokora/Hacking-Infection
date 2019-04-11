37fb7032c78751e28cc9c88adabe05dbceb9acd9
thunderbol <jdvid.bermudez@gmail.com>
2019-04-11 05:06:02 -0500

    platform

---
+++ Hacking Infection/Assets/Pruebas/Scripts/Cinematic.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Cinematic : MonoBehaviour
+{
+
+
+    public VideoPlayer videoplayer;
+
+
+    int num = 1;
+
+    float tiempo = 0;
+    float tiempoMax = 6;
+
+
+
+    private void Awake()
+    {
+        videoplayer = GetComponent<VideoPlayer>();
+        Video();
+    }
+
+    void Start()
+    {
+
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Video()
+    {
+
+        tiempo += Time.deltaTime;
+        Debug.Log(tiempo.ToString());
+
+        if (tiempo < tiempoMax)
+        {
+            videoplayer.Play();
+            num++;
+        }
+        if (num == tiempoMax)
+        {
+            videoplayer.Stop();
+            Debug.Log("Me pause");
+
+           
+        }
+
+        
+
+    }
+
+   
+}
