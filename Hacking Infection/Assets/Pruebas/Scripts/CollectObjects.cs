f7833df8edcb3964aee8cf6d38957fa49e28ad63
Thokora <fawergilthokora@hotmail.com>
2019-04-11 01:28:44 -0500

    Collect objects

---
+++ Hacking Infection/Assets/Pruebas/Scripts/CollectObjects.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectObjects : MonoBehaviour
+{
+    public int counterObjects;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Collectible")
+        {
+            counterObjects++;
+            Destroy(other.gameObject);
+        }
+    }
+}
